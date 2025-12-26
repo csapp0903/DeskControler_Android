a25f18d687d5ecdf55a8e33194b6bc6acf53811b
Guangxiao Li <liguangxiao3@gmail.com>
2025-12-26 17:42:33 +0800

    Add VideoDecoderWorker class header file

---
+++ VideoDecoderWorker.h
@@ -0,0 +1,47 @@
+#ifndef VIDEODECODERWORKER_H
+#define VIDEODECODERWORKER_H
+
+#include <QObject>
+#include <QByteArray>
+#include <QImage>
+
+// FFmpeg 相关头文件
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavutil/imgutils.h>
+#include <libavutil/opt.h>
+#include <libswscale/swscale.h>
+}
+
+#include <QQueue>
+#include <QMutex>
+#include <QTimer>
+
+class VideoDecoderWorker : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit VideoDecoderWorker(QObject* parent = nullptr);
+    ~VideoDecoderWorker();
+    void cleanup();
+
+public slots:
+    void decodePacket(const QByteArray& packetData);
+    void decodePacket1(const QByteArray& packetData);
+
+signals:
+    void frameDecoded(const QImage& image);
+
+private:
+    const AVCodec* codec = nullptr;
+    AVCodecContext* codecCtx = nullptr;
+    AVFrame* frame = nullptr;
+    SwsContext* swsCtx = nullptr;
+
+    mutable QMutex m_mutex;
+    QQueue<QByteArray> m_queue;
+    QTimer m_timer;
+};
+
+#endif // VIDEODECODERWORKER_H
